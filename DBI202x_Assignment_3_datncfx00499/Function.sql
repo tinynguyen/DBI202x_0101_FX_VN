380e8dcb8c21020a8fe7adbd3e3d01ce735f68bb
TinyNguyen <ncd.ccpn@gmail.com>
2019-07-09 21:17:26 +0700

    Project 3

--- DBI202x_Assignment_3_datncfx00499/Function.sql
+++ DBI202x_Assignment_3_datncfx00499/Function.sql
@@ -1,12 +1,16 @@
+-- Tạo 1 function nhận tham số truyền vào là ChannelId, trả về tổng số subscribe của một Channel
 create function SF_TotalSubscribe(@ChannelId int)
     returns table as
         return
+        -- đếm toàn bộ các subscriber trong bảng subscribe của một channel thông qua ChannelId
         select count(*) 'Total Subscribes'
         from Subscribe S
         where ChannelId = @ChannelId
 go
 
+-- Định nghĩa biến ChannelId
 declare @ChannelId int
+-- Gán giá trị vào biến ChannelId từ bảng Channel với Username là 'taylor-swift'
 select @ChannelId = ChannelId from Channel where Username = 'taylor-swift'
 select * from SF_TotalSubscribe(@ChannelId)
 go
