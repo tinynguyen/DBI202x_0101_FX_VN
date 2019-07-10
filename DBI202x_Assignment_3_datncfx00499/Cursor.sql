5c304e9abbfcd193ce5f5529c7e2737e78cd4d3f
TinyNguyen <ncd.ccpn@gmail.com>
2019-07-10 08:12:37 +0700

    Project 3

--- DBI202x_Assignment_3_datncfx00499/Cursor.sql
+++ DBI202x_Assignment_3_datncfx00499/Cursor.sql
@@ -2,24 +2,31 @@
 use Project3_DBI202x
 go
 
+-- Thêm trường Trend trong bảng Video
 alter table [Video]
     add [Trend] tinyint null
 go
 
+-- Tạo cursor với giá trị VideoId, Views từ bảng Video
 declare Cursor_CheckTrend cursor for
     select [VideoId], [Views]
     from [Video]
+-- Mở cursor
 open Cursor_CheckTrend
 
+-- Định nghĩa 2 biến VideoId, Views
 declare
     @VideoId int
 declare
     @Views bigint
 
+-- Duyệt phần tử đầu tiên trong cursor
 fetch next from Cursor_CheckTrend into @VideoId, @Views
 
+-- Vòng lập duyệt qua các phần từ của cursor
 while @@FETCH_STATUS = 0
 begin
+    -- Kiểm tra điều kiện và cập nhật giá trị cho Trend
     if @Views > 1000000000
         begin
             update [Video] set [Trend] = 1 where [VideoId] = @VideoId
@@ -46,5 +53,6 @@ begin
     fetch next from Cursor_CheckTrend into @VideoId, @Views
 end
 
+-- Đóng cursor
 close Cursor_CheckTrend
 deallocate Cursor_CheckTrend
